2588dc2ea1206c8300ea53be2916b88d690e9800
Patrick Outler <outler.patrick@gmail.com>
2018-05-19 02:00:51 -0400

    Add Netlify CMS
    Adjusted cms.js; more config and code necessary

---
+++ src/cms/cms.js
@@ -0,0 +1,6 @@
+import CMS from "netlify-cms";
+
+// This file is also where you would register preview templates
+// The template example includes a sibling dir with said templates.
+
+CMS.registerPreviewStyle("/styles.css");
